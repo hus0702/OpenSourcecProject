8ce4b6cfaf19496bc61a4dcd9ff5ab368a32cb38
smg-927 <godholy927@gmail.com>
2024-11-03 15:58:31 +0900

    3차 발표 ppt 작업 전 플레이어 시스템 커밋
    1. 플레이어 Input System new version으로 세팅
    2. 아트작업 전까지 사용할 플레이어 에셋 추가 설정
    3. 유석님 올려주신 영상 기반으로 Playerstate 클래스 틀 설정

---
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+
+    public Vector2 MovementInput { get; private set; }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        MovementInput = context.ReadValue<Vector2>();
+    }
+
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+    
+    }
+}
---
+++ Assets/Scripts/Player/PlayerStates/SubStates/PlayerIdleState.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerIdleState : PlayerGroundedState
+{
+    public PlayerIdleState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+
+    public override void DoCheck()
+    {
+        base.DoCheck();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (input.x != 0f)
+        {
+            stateMachine.ChangeState(player.MoveState);
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
