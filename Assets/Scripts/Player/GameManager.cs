f5f2e1b9f4bde1fe83f956d68e9a0e9a8e1f9caa
smg-927 <godholy927@gmail.com>
2024-11-24 18:51:12 +0900

    GameManager Assignauthority 함수 삭제
    ㅈㄱㄴ

--- Assets/Scripts/Player/GameManager.cs
+++ Assets/Scripts/Player/GameManager.cs
@@ -22,49 +22,4 @@ public class GameManager : NetworkBehaviour
         }
     }
 
-    public override void OnStartServer()
-    {
-        base.OnStartServer();
-        Debug.Log("나는 서버야");
-    }
-
-    public override void OnStartClient()
-    {
-        base.OnStartClient();
-        Debug.Log("클라이언트인데유");
-    }
-
-    [Server]
-    public void AssignAuthority(NetworkConnectionToClient conn)
-    {
-        // NetworkIdentity 컴포넌트가 필요함
-        NetworkIdentity networkIdentity = GetComponent<NetworkIdentity>();
-
-        if (networkIdentity != null && conn != null)
-        {
-            networkIdentity.AssignClientAuthority(conn);
-            Debug.Log("권한이 클라이언트에게 부여되었습니다.");
-        }
-        else
-        {
-            Debug.LogError("NetworkIdentity를 찾을 수 없거나 유효하지 않은 클라이언트 연결입니다.");
-        }
-    }
-
-    //[Server]
-    //public void RemoveAuthority(NetworkConnectionToClient conn)
-    //{
-    //    // NetworkIdentity 컴포넌트가 필요함
-    //    NetworkIdentity networkIdentity = GetComponent<NetworkIdentity>();
-
-    //    if (networkIdentity != null && conn != null)
-    //    {
-    //        networkIdentity.RemoveClientAuthority(conn);
-    //        Debug.Log("클라이언트 권한이 제거되었습니다.");
-    //    }
-    //    else
-    //    {
-    //        Debug.LogError("NetworkIdentity를 찾을 수 없거나 유효하지 않은 클라이언트 연결입니다.");
-    //    }
-    //}
 }
