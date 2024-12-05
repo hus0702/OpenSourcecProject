cdfea484bcd7af7994695a727b99ce0d0dbb36a3
hus0702 <81548468+hus0702@users.noreply.github.com>
2024-12-05 15:01:16 +0900

    오브젝트 풀 구조 구현, 소리 만들기 매니저 클래스만 만들음

---
+++ Assets/Scripts/유석파트/SoundDetectSystem/SoundMakeManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SoundMakeManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public ObjectPool soundMakeNodePool;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
