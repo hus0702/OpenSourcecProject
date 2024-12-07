aa3450605d273917bbc2813b6467ecaf03746b7f
Chanwoo <vcksdn330@naver.com>
2024-12-07 15:06:13 +0900

    구조완성

---
+++ Assets/Scripts/Enemy/State Machine/Entity.cs
@@ -0,0 +1,71 @@
+using Mirror;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Entity : NetworkBehaviour
+{
+    public FiniteStateMachine stateMachine;
+
+    public D_Entity entityData;
+    public int facingDirection{get; private set;}
+    public Rigidbody2D rb {get; private set;}
+    public Animator anim { get; private set;}
+    public GameObject aliveGO { get; private set;}
+
+    [SerializeField]
+    private Transform wallCheck;
+    [SerializeField]
+    private Transform ledgeCheck;
+
+    private Vector2 velocityWorkspace;
+    
+    public virtual void Start()
+    {
+        facingDirection = 1;
+
+        aliveGO = transform.Find("Alive").gameObject;
+        rb = aliveGO.GetComponent<Rigidbody2D>();
+        anim = aliveGO.GetComponent<Animator>();
+
+        stateMachine = new FiniteStateMachine();
+    }
+
+    public virtual void Update()
+    {
+        stateMachine.currentState.LogicUpdate();
+    }
+
+    public virtual void FixedUpdate()
+    {
+        stateMachine.currentState.PhysicsUpdate();
+    }
+
+    public virtual void SetVelocity(float velocity)
+    {
+        velocityWorkspace.Set(facingDirection * velocity, rb.linearVelocityY);
+        rb.linearVelocity = velocityWorkspace;
+    }
+
+    public virtual bool CheckWall()
+    {
+        return Physics2D.Raycast(wallCheck.position, aliveGO.transform.right, entityData.wallCheckDistance, entityData.whatIsGround);
+    }
+
+    public virtual bool CheckLedge()
+    {
+        return Physics2D.Raycast(ledgeCheck.position, Vector2.down, entityData.ledgeCheckDistance, entityData.whatIsGround);
+    }
+
+    public virtual void Flip()
+    {
+        facingDirection *= -1;
+        aliveGO.transform.Rotate(0f, 180f, 0f);
+    }
+
+    private void OnDrawGizmos() {
+        
+        Gizmos.DrawLine(wallCheck.position, wallCheck.position * (Vector2.right * facingDirection * entityData.wallCheckDistance));
+        Gizmos.DrawLine(ledgeCheck.position, ledgeCheck.position * (Vector2.down * entityData.wallCheckDistance));
+    }
+
+}
