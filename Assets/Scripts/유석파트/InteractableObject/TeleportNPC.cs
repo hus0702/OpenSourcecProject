760cb31f93cdb240db3f9b79b41d10aaa679c1b4
hus0702 <81548468+hus0702@users.noreply.github.com>
2024-12-07 17:48:13 +0900

    이동 테스트

--- Assets/Scripts/유석파트/InteractableObject/TeleportNPC.cs
+++ Assets/Scripts/유석파트/InteractableObject/TeleportNPC.cs
@@ -4,8 +4,9 @@ public class TeleportNPC : InteractableObject
 {
     public override bool CheckInteractable(GameObject requester)
     {
+        if(GameManager.instance.Pdcontainer.iscarrying) return true;
         // 플레이어 1이 업고 있는 상태가 아니라면 false 리턴
-        return false;
+        else return false;
     }
 
     public InteractFailTalkBubble failHandler;
@@ -16,7 +17,11 @@ public class TeleportNPC : InteractableObject
     public override void ExecuteOnSuccess(GameObject requester)
     {
         {
-            myChanger.StartScene();
+            //myChanger.StartScene();
+        
+            //34.5 , 11.5
+
+            requester.transform.position = new Vector3(34.5f, 11.5f, 0f);
         }
     }
 
