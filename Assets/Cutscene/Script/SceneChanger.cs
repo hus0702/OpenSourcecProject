84c9d436780af09b818785bf1caa28a851f85922
hus0702 <81548468+hus0702@users.noreply.github.com>
2024-12-07 16:21:10 +0900

    씬체인저 업데이트 3

--- Assets/Cutscene/Script/SceneChanger.cs
+++ Assets/Cutscene/Script/SceneChanger.cs
@@ -21,26 +21,18 @@ public class SceneChanger : MonoBehaviour, ITriggered
     private void BeforeStartScene()
     {
         SetPlayerSpawnPositionOnLoad();
-
-        /*
-            이 뒤로 자유롭게 작성하시면 될 것 같습니다.
-        */
     }
 
     private void SetPlayerSpawnPositionOnLoad()
-    {
-        PlayerObjectController player = NetworkClient.localPlayer.GetComponent<PlayerObjectController>();
-
-        if(player == null) {Debug.LogError("현재 할당된 로컬 플레이어가 존재하지 않습니다."); return;}
-        
-        if(player.Role == PlayerObjectController.Blind) GameManager.instance.SpawnPositionOnLoad = spawnPointOnLoadForBlind;
-        else GameManager.instance.SpawnPositionOnLoad = spawnPointOnLoadForLimp;
+    {        
+        GameManager.instance.BlindSpawnPositionOnLoad = spawnPointOnLoadForBlind;
+        GameManager.instance.LimpSpawnPositionOnLoad = spawnPointOnLoadForLimp;
     }
 
     public void StartScene()
     {
         BeforeStartScene();
-        
+
         Manager.StartGame(sceneName);
         
         //SceneManager.LoadScene(sceneName);
