35e527d967a9c5d00a35f543a3528d16c6c4bd38
hus0702 <81548468+hus0702@users.noreply.github.com>
2024-12-17 01:27:58 +0900

    벤트 코드 수정

--- Assets/Scripts/유석파트/InteractableObject/개별 객체 코드/Vent.cs
+++ Assets/Scripts/유석파트/InteractableObject/개별 객체 코드/Vent.cs
@@ -24,7 +24,7 @@ public class Vent : Door
     {
         if(!isOpened)
         {
-            if(requester.tag == "Limb" && GameManager.instance.Ldcontainer.itemset[3] || requester.tag == "Blind" && GameManager.instance.Pdcontainer.itemset[3])
+            if(requester.tag == "Limb" && GameManager.instance.Ldcontainer.itemset[3])
             {
                 return true;
             }
