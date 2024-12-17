9ba150c0f9152805badfca89f900da5883d6c8a4
hus0702 <81548468+hus0702@users.noreply.github.com>
2024-12-18 04:48:54 +0900

    자잘한 수정

--- Assets/Scripts/유석파트/InteractableObject/개별 객체 코드/DoorNeedTogether.cs
+++ Assets/Scripts/유석파트/InteractableObject/개별 객체 코드/DoorNeedTogether.cs
@@ -5,8 +5,8 @@ public class DoorNeedTogether : Door
     public override bool CheckInteractable(GameObject requester)
     {
         // 만약 두 명이 아니라면
-        //if(GameManager.instance.Pdcontainer.iscarrying) return false;
+        if(GameManager.instance.Pdcontainer.iscarrying) return false;
         //else return base.CheckInteractable(requester);
-        return base.CheckInteractable(requester);
+        else return base.CheckInteractable(requester);
     }
 }
