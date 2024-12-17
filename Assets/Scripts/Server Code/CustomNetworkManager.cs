63ce0173bcaa3d7b3f7cb87b9dd8c1fb8b36cc30
smg-927 <godholy927@gmail.com>
2024-12-18 03:41:23 +0900

    애니미 스폰 구현

--- Assets/Scripts/Server Code/CustomNetworkManager.cs
+++ Assets/Scripts/Server Code/CustomNetworkManager.cs
@@ -97,6 +97,12 @@ public class CustomNetworkManager : NetworkManager
 
     public void SpawnPrefabs()
     {
+        GameScenePrefab = Instantiate(spawnPrefabs[3], new Vector3(42, 5.2f,0) ,spawnPrefabs[3].transform.rotation); //러너 생성
+        NetworkServer.Spawn(GameScenePrefab);
+        GameScenePrefab = Instantiate(spawnPrefabs[4], new Vector3(47, -18f, 0), spawnPrefabs[4].transform.rotation); // 크롤러 생성
+        NetworkServer.Spawn(GameScenePrefab);
+        GameScenePrefab = Instantiate(spawnPrefabs[5], new Vector3(36, 12, 0), spawnPrefabs[5].transform.rotation); // 보스 생성
+        NetworkServer.Spawn(GameScenePrefab);
         foreach (var conn in NetworkServer.connections.Values)
         {
             PlayerObjectController playerObjectController = conn.identity.gameObject.GetComponent<PlayerObjectController>();
