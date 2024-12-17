5021477ad58569eda4af6b4d4a387d3e8daa9b41
hus0702 <81548468+hus0702@users.noreply.github.com>
2024-12-18 05:02:55 +0900

    두 명이서 열어야 하는 문 버그 수정

--- Assets/Scripts/유석파트/InteractableObject/개별 객체 코드/DoorNeedTogether.cs
+++ Assets/Scripts/유석파트/InteractableObject/개별 객체 코드/DoorNeedTogether.cs
@@ -5,7 +5,7 @@ public class DoorNeedTogether : Door
     public override bool CheckInteractable(GameObject requester)
     {
         // 만약 두 명이 아니라면
-        if(GameManager.instance.Pdcontainer.iscarrying) return false;
+        if(GameManager.instance.Pdcontainer.iscarrying) return true;
         //else return base.CheckInteractable(requester);
         else return base.CheckInteractable(requester);
     }
